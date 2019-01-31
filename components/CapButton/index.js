82fc2cd9fe61c47ce114342354ebb464aeb10f93
Jagrati Tomar <jagrati.tomar@capillarytech.com>
2019-01-31 17:59:36 +0530

    New files

---
+++ components/CapButton/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Button } from 'antd';
+
+class CapButton extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Button />
+            </div>
+        );
+    }
+}
+
+export default CapButton;
